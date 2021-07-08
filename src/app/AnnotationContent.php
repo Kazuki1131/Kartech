88ccbe456d22c8f69a605b4f40e78ec360402cdc
github kazuki1131 <onetsubyo@gmail.com>
2021-07-08 14:51:36 +0900

    モデルにリレーションを定義 #19

--- src/app/AnnotationContent.php
+++ src/app/AnnotationContent.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class AnnotationContent extends Model
 {
-    //
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function annotation_title()
+    {
+        return $this->belongsTo(AnnotationTitle::class);
+    }
 }
--- src/app/Customer.php
+++ src/app/Customer.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function biz_record()
+    {
+        return $this->hasMany(BizRecord::class);
+    }
+
+    public function annotation_content()
+    {
+        return $this->hasMany(AnnotationContent::class);
+    }
 }
