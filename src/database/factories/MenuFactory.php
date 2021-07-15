602c53777d0713793841918460f93d364fd82da7
github kazuki1131 <onetsubyo@gmail.com>
2021-07-16 08:37:29 +0900

    Model Factoryの作成 #20

---
+++ src/database/factories/MenuFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Menu;
+use Faker\Generator as Faker;
+
+$factory->define(Menu::class, function (Faker $faker) {
+    return [
+        'user_id' => $faker->numberBetween($min=1, $max=100),
+        'menu_name' => $faker->word,
+        'description' => $faker->realText,
+        'price' => $faker->randomNumber(4),
+    ];
+});
--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Customer;
+use App\Menu;
+use App\BizRecord;
+use App\AnnotationTitle;
+use App\AnnotationContent;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(User::class, 100)->create();
+        factory(Customer::class, 1000)->create();
+        factory(Menu::class, 1000)->create();
+        factory(BizRecord::class, 3000)->create();
+        factory(AnnotationTitle::class, 500)->create();
+        factory(AnnotationContent::class, 1500)->create();
     }
 }
