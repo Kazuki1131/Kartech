8fcdccf980c17932b8c5836383a64b76c92887bc
github kazuki1131 <onetsubyo@gmail.com>
2021-09-02 14:16:34 +0900

    アンケートのフォームリクエストの作成 #70

---
+++ src/app/Http/Requests/QuestionnaireRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+use Auth;
+
+class QuestionnaireRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(Request $request)
+    {
+        return [
+            'item' => 'required|string|max:100',
+            'type' => 'required|digits:1',
+            'singleAnswers.*' => 'nullable|string|max:100',
+            'multipleAnswers.*' => 'nullable|string|max:100',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'item' => 'アンケート内容',
+            'type' => 'アンケートタイプ',
+            'singleAnswers.*' => '選択肢',
+            'multipleAnswers.*' => '選択肢',
+        ];
+    }
+}
