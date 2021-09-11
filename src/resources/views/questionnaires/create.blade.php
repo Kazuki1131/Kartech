1093e5608eca446166a5b2c58fdb57efb86f59f1
github kazuki1131 <onetsubyo@gmail.com>
2021-09-11 11:59:37 +0900

    questionnaires.createのレイアウト調整 #91

--- src/resources/views/questionnaires/create.blade.php
+++ src/resources/views/questionnaires/create.blade.php
@@ -1,60 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="card mx-auto w-100 font-origin-head">
-        <div class="card-header font-origin-title">
-            <h3 class="text-center"><i class="fas fa-pen mr-2"></i>お客様アンケート新規作成</h3>
-        </div>
+<div class="container pb-5">
+    <div class="text-right">
+        <a class="btn-origin-return" href="{{ route('customers.index') }}">戻る</a>
+    </div>
+    <div class="card mx-auto w-100 bg-origin-card">
+        <div class="card-header card-head-origin">お客様アンケート新規作成</div>
         <div class="card-body mx-auto w-75">
             <form action="{{ route('questionnaires.store') }}" method="POST" autocomplete="off">
                 @csrf
                 <div class="form-group mt-4">
-                    <label for="questionnaire" class="mb-0"><h5>アンケート内容</h5></label>
+                    <label for="questionnaire" class="control-label">アンケート内容</label>
                     <input class="form-control" id="questionnaire" name="item"
                     placeholder="例：ご来店のきっかけ" value="{{ old('item') }}">
                 </div>
                 @error('item')
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
-                <div class="form-group mt-4 mb-0">
-                    <label class="control-label m-0"><h5>アンケートのタイプ</h5></label>
-                </div>
-                <div class="form-check form-check-inline">
-                    <input class="js-check form-check-input" id="typeDescription" type="radio" name="type"
-                    value="0" onclick="formSwitch()" {{ old('type') === '0' ? 'checked' : '' }}>
-                    <label for="typeDescription" class="form-check-label">記述式</label>
-                </div>
-                <div class="form-check form-check-inline">
-                    <input class="js-check form-check-input" id="typeSingle" type="radio" name="type"
-                    value="1" onclick="formSwitch()" {{ old('type') === '1' ? 'checked' : '' }}>
-                    <label for="typeSingle" class="form-check-label">選択式(単一回答のみ)</label>
-                </div>
-                <div class="form-check form-check-inline">
-                    <input class="js-check form-check-input" id="type_multiple" type="radio" name="type"
-                    value="2" onclick="formSwitch()" {{ old('type') === '2' ? 'checked' : '' }}>
-                    <label for="type_multiple" class="form-check-label">選択式(複数回答可)</label>
+                <div class="form-group mt-4">
+                    <label class="control-label">アンケートのタイプ</label>
+                    <div class="form-check form-check">
+                        <input class="js-check form-check-input" id="typeDescription" type="radio" name="type"
+                        value="0" onclick="formSwitch()" {{ old('type') === '0' ? 'checked' : '' }}>
+                        <label for="typeDescription" class="form-check-label">記述式</label>
+                    </div>
+                    <div class="form-check form-check">
+                        <input class="js-check form-check-input" id="typeSingle" type="radio" name="type"
+                        value="1" onclick="formSwitch()" {{ old('type') === '1' ? 'checked' : '' }}>
+                        <label for="typeSingle" class="form-check-label">選択式(単一回答のみ)</label>
+                    </div>
+                    <div class="form-check form-check">
+                        <input class="js-check form-check-input" id="type_multiple" type="radio" name="type"
+                        value="2" onclick="formSwitch()" {{ old('type') === '2' ? 'checked' : '' }}>
+                        <label for="type_multiple" class="form-check-label">選択式(複数回答可)</label>
+                    </div>
                 </div>
                 @error('type')
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
                 <div id="singleAnswer" class="mt-4">
-                    <div>選択肢を登録してください。</div>
-                    <div id="inputSingleAnswer" class="answerStyle">
+                    <label>選択肢を登録してください。</label>
+                    <div id="inputSingleAnswer" class="answer-style">
                         <input type="text" class="q_option" name="singleAnswers[0]"
                         placeholder="選択肢(単一回答式)" value="{{ old('singleAnswers.0') }}">
-                        <input type="button" value="－" class="deleteSingleAnswer pluralBtn">
+                        <input type="button" value="－" class="deleteSingleAnswer plural-btn">
                     </div>
-                    <input type="button" value="＋" id="addSingle" class="addSingleAnswer pluralBtn">
+                    <input type="button" value="＋" id="addSingle" class="addSingleAnswer plural-btn">
                 </div>
                 <div id="multipleAnswers" class="mt-4">
-                    <div>選択肢を登録してください。</div>
-                    <div id="inputMultipleAnswer" class="answerStyle">
+                    <label>選択肢を登録してください。</label>
+                    <div id="inputMultipleAnswer" class="answer-style">
                         <input type="text" class="q_option" name="multipleAnswers[0]"
                         placeholder="選択肢(複数回答式)" value="{{ old('multipleAnswers.0') }}">
-                        <input type="button" value="－" class="deleteMultipleAnswer pluralBtn">
+                        <input type="button" value="－" class="deleteMultipleAnswer plural-btn">
                     </div>
-                    <input type="button" value="＋" id="addMultiple" class="addMultipleAnswer pluralBtn">
+                    <input type="button" value="＋" id="addMultiple" class="addMultipleAnswer plural-btn">
                 </div>
                 @error('singleAnswers.*')
                     <div class="text-danger">{{ $message }}</div>
@@ -62,8 +63,7 @@
                 @error('multipleAnswers.*')
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
-                <button type="submit" class="btn btn-origin d-block mx-auto mt-4">登録する</button>
-                <div class="return"><a class="btn bg-origin-btn mt-3 px-4" href="{{ route('customers.index') }}">戻る</a></div>
+                <button type="submit" class="btn-origin d-block mx-auto mt-4">登録する</button>
             </form>
         </div>
     </div>
