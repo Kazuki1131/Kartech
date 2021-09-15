6c1149a4d8f7c58b61f8ba34127b735202be0648
github kazuki1131 <onetsubyo@gmail.com>
2021-09-15 11:42:58 +0900

    顧客詳細ページに販売したサービスを表示する処理を追加

--- src/resources/views/customers/show.blade.php
+++ src/resources/views/customers/show.blade.php
@@ -2,10 +2,9 @@
 
 @section('content')
 <div class="container pb-5">
-    <a href="{{ route('visited_records.create', ['customer_id' => $customer->id]) }}">
-        <button type="button" class="btn btn-origin float-right mb-2">来店記録の作成</button>
-    </a>
-    <div class="card mx-auto w-100 bg-origin-card">
+    <a class="btn-origin-return float-left" href="{{ route('customers.index') }}">戻る</a>
+    <a href="{{ route('visited_records.create', ['customer_id' => $customer->id]) }}" class="btn-origin float-right mb-2">来店記録の作成</a>
+    <div class="card mx-auto w-100 bg-origin-card px-0">
         <div class="card-header card-head-origin py-4">
                 <i class="fas fa-user m-2 font-weight-bold text-secondary"></i>
                 {{ $customer->name ?? '' }}（{{ $customer->name_kana ?? ''}}）
@@ -16,7 +15,7 @@
                 <div class="col-md-6">
                     <ul>
                         <li class="origin-li-top">顧客番号：{{ $customer->control_number ?? '' }}</li>
-                        <li class="origin-li">生年月日：{{ $customer->birthday ?? '' }}</li>
+                        <li class="origin-li">生年月日：{{ $customer->birthday ?? '未登録' }}</li>
                         <li class="origin-li">電話番号：{{ $customer->tel ?? '' }}</li>
                         <li class="origin-li">
                             性別：{{ $customer->gender ?  $customer->gender === 1 ? '女性' : '男性' : ''}}
@@ -25,8 +24,8 @@
                 </div>
                 <div class="col-md-6">
                     <ul>
-                        <li class="origin-li-top">最終来店日：{{ $lastVisitDate ?? '' }}</li>
-                        <li class="origin-li">総来店回数：{{ $visitedTimes ?? 0 }}回</li>
+                        <li class="origin-li-top">最終来店日：{{ $visitedRecords[0]->visited_at ?? '' }}</li>
+                        <li class="origin-li">総来店回数：{{ count($visitedRecords) }}回</li>
                         <li class="origin-li">平均単価：{{ $avgPurchasePrices ?? 0 }}円</li>
                     </ul>
                 </div>
@@ -64,35 +63,33 @@
             </div>
         @endif
         @if($visitedRecords)
-            <div class="card-body h6 mb-0 px-0">
+            <div class="card-body h6 px-0">
                 <h2 class="text-center title">ー 来店記録 ー</h2>
                 @foreach($visitedRecords as $visitedRecord)
-                    <div class="py-4 mb-1">
-                        <div class="px-4">
-                            @foreach($imagePaths as $recordId => $imagePath)
-                                @if($visitedRecord->id === $recordId)
-                                    <div class="h4 ml-2">
-                                        {{ $visitedRecord->visited_at ?? '来店日未登録' }}
-                                    </div>
-                                    <div class="image">
-                                        @foreach($imagePath as $image)
-                                            <div class="image_mouseover">
-                                                <img src="{{ Storage::disk('s3')->url($image) }}" alt="image_{{ $recordId }}">
-                                            </div>
-                                        @endforeach
-                                    </div>
+                    @foreach($imagePaths as $recordId => $imagePath)
+                        @if($visitedRecord->id === $recordId)
+                            <div class="h4 ml-2 subtitle">
+                                【{{ $visitedRecord->visited_at ?? '来店日未登録' }}】
+                            </div>
+                            <div class="image">
+                                @foreach($imagePath as $image)
+                                    <img src="{{ Storage::disk('s3')->url($image) }}" alt="image_{{ $recordId }}">
+                                @endforeach
+                            </div>
+                        @endif
+                    @endforeach
+                    <div class="row ml-2 my-4">
+                        <div class="col-md-6 mb-4">
+                            <div class="subtitle">- 接客メモ -</div>
+                            <div class="ml-2 text">{{ $visitedRecord->memo ?? '未登録' }}</div>
+                        </div>
+                        <div class="col-md-6 mb-4">
+                            <div class="subtitle">- 提供したサービス -</div>
+                            @foreach($servicesSold as $service)
+                                @if($visitedRecord->id === $service->visited_id)
+                                    <div class="ml-2 text">・{{ $service->menu_name }}（¥{{ number_format($service->price_sold) }}）</div>
                                 @endif
                             @endforeach
-                        </div>
-                        <div class="row ml-2">
-                            <div class="col-md-6 mb-4">
-                                <div class="h4">- 接客メモ -</div>
-                                <div class="ml-2 text">{{ $visitedRecord->memo ?? '未登録' }}</div>
-                            </div>
-                            <div class="col-md-6 mb-4">
-                                <div class="h4">- 提供したサービス -</div>
-                                <div class="ml-2 text">メニューA</div>
-                            </div>
                         </div>
                     </div>
                 @endforeach
@@ -100,7 +97,6 @@
         @endif
     </div>
     <div class="text-center mt-4">
-        <a class="btn-origin-return" href="{{ route('customers.index') }}">戻る</a>
     </div>
 </div>
 @endsection
